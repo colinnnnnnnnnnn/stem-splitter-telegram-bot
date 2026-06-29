ab411d23a56873a653baa4abfdaf8b4d4eb25639
calin <kalinpoiata@gmail.com>
2026-06-29 13:56:55 +0300

    Create download_audio function in order to split functionalities

--- download_mp3.py
+++ download_mp3.py
@@ -1,39 +1,48 @@
 import os
 import subprocess
 import uuid
+from dataclasses import dataclass
 from pathlib import Path
-from tempfile import TemporaryDirectory
+
+
+@dataclass
+class DownloadResult:
+    jobid: str
+    path: Path
+    title: str
+
 
 BASE = Path(__file__).resolve().parent
-TMP_PARENT = BASE / "data" / "tmp"
-INPUT_DIR = BASE / "data" / "incoming"
-TMP_PARENT.mkdir(parents=True, exist_ok=True)
-INPUT_DIR.mkdir(parents=True, exist_ok=True)
+DOWNLOAD_DIR = BASE / "data" / "downloaded"
+INCOMING_DIR = BASE / "data" / "incoming"
+DOWNLOAD_DIR.mkdir(parents=True, exist_ok=True)
+INCOMING_DIR.mkdir(parents=True, exist_ok=True)
 
 
-def download_mp3(url: str) -> tuple[str, str]:
-    jobid = uuid.uuid4().hex
-    with TemporaryDirectory(dir=TMP_PARENT) as td:
-        try:
-            res = subprocess.run(
-                [
-                    "yt-dlp",
-                    "--extract-audio",
-                    "--audio-format",
-                    "mp3",
-                    "--print",
-                    "after_move:filepath",
-                    "-o",
-                    os.path.join(td, "%(title)s.%(ext)s"),
-                    url,
-                ],
-                capture_output=True,
-                text=True,
-                check=True,
-            )
-            downloaded = res.stdout.strip().splitlines()[-1]
-            final = Path(INPUT_DIR) / f"{jobid}.mp3"
-            os.replace(downloaded, final)
-            return str(final), jobid
-        except subprocess.CalledProcessError as e:
-            raise RuntimeError(f"yt-dlp failed: {e.stderr}")
+def download_audio(url: str, jobid: str) -> DownloadResult:
+    job_path = DOWNLOAD_DIR / jobid
+    try:
+        res = subprocess.run(
+            [
+                "yt-dlp",
+                "--extract-audio",
+                "--audio-format",
+                "mp3",
+                "--print",
+                "after_move:filepath",
+                "-o",
+                os.path.join(job_path, "%(title)s.%(ext)s"),
+                url,
+            ],
+            capture_output=True,
+            text=True,
+            check=True,
+        )
+
+        res_path = Path(res.stdout.strip().splitlines()[-1])
+        song_title = res_path.stem
+        return DownloadResult(jobid=jobid, path=res_path, title=song_title)
+    except subprocess.CalledProcessError as e:
+        raise RuntimeError(f"yt-dlp failed: {e.stderr}")
+
+
